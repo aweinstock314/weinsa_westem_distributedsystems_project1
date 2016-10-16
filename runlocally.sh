ace76941d97e90590094feff0ad43e5699cd9ab9
Avi Weinstock <aweinstock314@gmail.com>
2016-10-16 18:30:14 -0400

    Add RUST_BACKTRACE/debug logging to runlocally.sh.

--- runlocally.sh
+++ runlocally.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export RUST_BACKTRACE=1
 pkill weinsa_westem_d # pkill has a 15-char length limit for some reason
-#cargo build && (for i in $(seq 1 6); do (./target/debug/weinsa_westem_distributedsystems_project1 $i &); done)
+#cargo build && (for i in $(seq 1 6); do (./target/debug/weinsa_westem_distributedsystems_project1 $i > log$i &); done)
 cargo build --release && (for i in $(seq 1 6); do (./target/release/weinsa_westem_distributedsystems_project1 $i > log$i &); done)
